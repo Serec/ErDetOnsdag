71a1b6d02bf35c2ac79f399002284c8ba0781662
Serec <soren.kaae@gmail.com>
2015-11-04 15:27:06 +0100

    what

---
+++ ErDetOnsdag/Controllers/IkkeOnsdagController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ErDetOnsdag.Controllers
+{
+    public class IkkeOnsdagController : Controller
+    {
+        // GET: IkkeOnsdag
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ErDetOnsdag/Controllers/OnsdagController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ErDetOnsdag.Controllers
+{
+    public class OnsdagController : Controller
+    {
+        // GET: Onsdag
+        public ActionResult Index()
+        {
+            string onsdag;
+            if(DateTime.Today.DayOfWeek == DayOfWeek.Wednesday)
+            {
+                onsdag = "Ja, og der er onsdagssnegle!";
+            }
+            else
+            {
+                onsdag = "Nej, det er " + DateTime.Today.DayOfWeek + ", din hat! Så der er ingen ";
+            }
+            ViewBag.Onsdag = onsdag;
+            return View();
+        }
+    }
+}
